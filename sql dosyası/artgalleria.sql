f08278523286ac51bf450062801dadceed138713
Merve Nur Uzun <mervenuru@gmail.com>
2023-03-05 22:11:32 +0300

    Create artgalleria.sql

---
+++ sql dosyası/artgalleria.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 05 Mar 2023, 20:07:33
+-- Sunucu sürümü: 10.4.24-MariaDB
+-- PHP Sürümü: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `artgalleria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `failed_jobs`
+--
+-- artgalleria.failed_jobs tablosu için yapı okuma hatası: #1932 - Table &#039;artgalleria.failed_jobs&#039; doesn&#039;t exist in engine
+-- artgalleria.failed_jobs tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `artgalleria`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kategori`
+--
+-- artgalleria.kategori tablosu için yapı okuma hatası: #1932 - Table &#039;artgalleria.kategori&#039; doesn&#039;t exist in engine
+-- artgalleria.kategori tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `artgalleria`.`kategori`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `migrations`
+--
+-- artgalleria.migrations tablosu için yapı okuma hatası: #1932 - Table &#039;artgalleria.migrations&#039; doesn&#039;t exist in engine
+-- artgalleria.migrations tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `artgalleria`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `password_resets`
+--
+-- artgalleria.password_resets tablosu için yapı okuma hatası: #1932 - Table &#039;artgalleria.password_resets&#039; doesn&#039;t exist in engine
+-- artgalleria.password_resets tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `artgalleria`.`password_resets`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `personal_access_tokens`
+--
+-- artgalleria.personal_access_tokens tablosu için yapı okuma hatası: #1932 - Table &#039;artgalleria.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- artgalleria.personal_access_tokens tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `artgalleria`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `sepet`
+--
+-- artgalleria.sepet tablosu için yapı okuma hatası: #1932 - Table &#039;artgalleria.sepet&#039; doesn&#039;t exist in engine
+-- artgalleria.sepet tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `artgalleria`.`sepet`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `siparis`
+--
+-- artgalleria.siparis tablosu için yapı okuma hatası: #1932 - Table &#039;artgalleria.siparis&#039; doesn&#039;t exist in engine
+-- artgalleria.siparis tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `artgalleria`.`siparis`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `siparis_item`
+--
+-- artgalleria.siparis_item tablosu için yapı okuma hatası: #1932 - Table &#039;artgalleria.siparis_item&#039; doesn&#039;t exist in engine
+-- artgalleria.siparis_item tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `artgalleria`.`siparis_item`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `urunler`
+--
+-- artgalleria.urunler tablosu için yapı okuma hatası: #1932 - Table &#039;artgalleria.urunler&#039; doesn&#039;t exist in engine
+-- artgalleria.urunler tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `artgalleria`.`urunler`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `users`
+--
+-- artgalleria.users tablosu için yapı okuma hatası: #1932 - Table &#039;artgalleria.users&#039; doesn&#039;t exist in engine
+-- artgalleria.users tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `artgalleria`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
